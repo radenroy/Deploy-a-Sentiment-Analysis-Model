c177f70daf7ff79517a03134d867a189394ee487
Raden Roy Pradana <45507440+radenroy@users.noreply.github.com>
2019-07-02 22:57:57 -0700

    edited the to do

--- serve/predict.py
+++ serve/predict.py
@@ -65,11 +65,7 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-    
+    # Process input_data so that it is ready to be sent to our model. 
     data_X = review_to_words(input_data)
     data_X, data_len = convert_and_pad(model.word_dict, data_X)
 
@@ -84,8 +80,8 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
+    # Compute the result of applying the model to the input data. The variable `result` should
+    # be a numpy array which contains a single integer which is either 1 or 0
     
     with torch.no_grad():
         result = model(data)
